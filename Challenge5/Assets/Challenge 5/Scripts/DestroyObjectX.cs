03e4457a32f51e32a0069f7fa64c608d40921daa
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-04-24 21:59:58 -0500

    Challenge5

---
+++ Challenge5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Gerard Lamoureux
+* Assignment 8
+* Handles Destruction of objects
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+}
